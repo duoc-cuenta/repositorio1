c7df0113a8d80e50f0b0fe40d6837338a03eeefb
duoc-cuenta <56618819+duoc-cuenta@users.noreply.github.com>
2025-06-13 16:13:32 -0400

    Update ej2.py
    Se corrige operador aritmético...

--- ej2.py
+++ ej2.py
@@ -1,7 +1,8 @@
 try:
     n1= 7
     n2= 0
-    resultado = n1 / n2
+    # resultado = n1 / n2
+    resultado = n1 * n2
 except Exception as ex:
     print("ha ocurrido  un error: ")
     print (ex)
