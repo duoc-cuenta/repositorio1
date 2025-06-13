fbc7db51b37446d826942ab45ee86127b55cac75
duoc-cuenta <56618819+duoc-cuenta@users.noreply.github.com>
2025-06-13 16:07:11 -0400

    Se agrega archivo con funcionalidad pop()
    Se explica cómo eliminar elementos de un diccionario con funcionalidad pop()

---
+++ ej1.py
@@ -0,0 +1,12 @@
+diccionario = {
+    "marca":"nissan",
+    "modelo":"kicks",
+    "color":"blanco",
+    "kilometros":100,
+    "estado":True,
+    "sucursal":['La Florida','Plaza Tobalaba','Plaza Vespucio']
+}
+
+# eliminar un elemento
+diccionario.pop("color") # se utiliza la KEY para eliminar
+print(diccionario)
